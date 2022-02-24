58fcda82da7fe877d3cd43ec132a83aa076e35eb
yousefQJ19 <yousefnajeh3@gmail.com>
2022-02-24 05:48:00 +0200

    يماااا تعبت يزم ضايل جافا بس

---
+++ js/master.js
@@ -0,0 +1,12 @@
+let arryofinput=document.querySelectorAll('.rech p')
+
+for (let i=0;i<arryofinput.length;i++)
+{
+    console.log(arryofinput[i])
+    //arryofinput[i].style.display='block'
+}
+
+
+let signup=document.getElementsByClassName('signup');
+
+signup.addEventListener("click",action);
