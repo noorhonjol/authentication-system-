c172212f57c509c87ddd04382aacf904fe9e802d
noorhonjol <nooralhongal@gmail.com>
2022-02-28 13:29:02 +0200

    noor

--- node.js/server.js
+++ node.js/server.js
@@ -1,21 +1,43 @@
 const express = require('express');
-const { status } = require('express/lib/response');
-const httptornedo = express();
-const db= require('./db');
+
+const app = express();
+
+// const db= require('./db');
+
+const expresslayout=require('express-ejs-layouts');
+
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname,+'public/css'))
+
+
+app.use(expresslayout);
+app.set('view engine','ejs')
 
 
 
-httptornedo.post('/one',(req,res)=>{
-    let one = req.body
-    try{
-        let status=db.addone(one);
-        res.status(201).json(status);
-    }
-    catch(error){
-        res.status(500).json({status:"Erorr", message:"something wrong"});
-    }
+app.get('',(req,res)=>{
+    res.render('index')
+
+})
+app.get('/sign-up',(req,res)=>{
+    res.render('sign-up')
+})
+app.get('/login',(req,res)=>{
+
+    res.render("login-in")
 })
 
-httptornedo.listen(3200, ()=>{
+// app.post('/one',(req,res)=>{
+//     let one = req.body
+//     try{
+//         let status=db.addone(one);
+//         res.status(201).json(status);
+//     }
+//     catch(error){
+//         res.status(500).json({status:"Erorr", message:"something wrong"});
+//     }
+// })
+
+app.listen(3200, ()=>{
     console.log("server is run");
 })
